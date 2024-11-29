b2f3a6ed3a8bbc1ac1d365aab082f4e86b99cbaa
Saraliev Khalid <saraliev_halid@mail.ru>
2024-11-30 00:57:46 +0300

    added Seminar2:task2

---
+++ Seminar2/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите координаты точки X ");
+int coordinatesX = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты точки Y ");
+int coordinatesY = Convert.ToInt32(Console.ReadLine());
+
+ if (coordinatesX > 0 && coordinatesY > 0)
+ {
+     Console.WriteLine($"Номер координатной четверти {coordinatesX} и {coordinatesY} равно 1");
+ }
+ else if (coordinatesX < 0 && coordinatesY > 0)
+ {
+     Console.WriteLine($"Номер координатной четверти {coordinatesX} и {coordinatesY} равно 2");
+ }
+ else if (coordinatesX < 0 && coordinatesY < 0)
+{
+     Console.WriteLine($"Номер координатной четверти {coordinatesX} и {coordinatesY} равно 3");
+}
+else if ( coordinatesX > 0 && coordinatesY < 0)
+{
+    Console.WriteLine($"Номер координатной четверти {coordinatesX} и {coordinatesY} равно 4");
+}
+else
+{
+    Console.WriteLine("Некорректный ввод!");
+}
