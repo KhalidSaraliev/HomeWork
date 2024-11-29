51f17222f9aef22c7471c43472ac6f2d29c80cdd
Saraliev Khalid <saraliev_halid@mail.ru>
2024-11-30 01:19:58 +0300

    added seminar2:task3

---
+++ Seminar2/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите первое двухзначное число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе двухзначное число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+if ((num1 > 9 && num1 < 100) && (num2 > 9 && num2 < 100))
+{
+    if (num1 > num2)
+    {
+        Console.WriteLine($"Число {num1} больше чем {num2}");
+    }
+    else
+    {
+        Console.WriteLine($"Число {num2} больше чем {num1}");
+    }
+}
+else
+{
+    Console.WriteLine("Некорректный ввод");
+}
