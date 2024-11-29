efee29053f76bdc1ca18a92bad32cd4a78e26b07
Saraliev Khalid <saraliev_halid@mail.ru>
2024-11-30 00:32:09 +0300

    added Seminar2:task1

---
+++ Seminar2/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = 7;
+int num2 = 23;
+if (num % num1 == 0)
+{
+    Console.WriteLine($"Да, {num} кратно {num1}");
+}
+else
+{
+    Console.WriteLine($"Нет, {num} не кратно {num1}");
+}
+if (num % num2 == 0)
+{
+    Console.WriteLine($"Да, {num} кратно {num2}");
+}
+else
+{
+    Console.WriteLine($"Нет, {num} не кратно {num2}");
+}
