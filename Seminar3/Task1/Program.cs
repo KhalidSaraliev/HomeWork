f1eac6587708a75f7949987d383fe80046689030
Saraliev Khalid <saraliev_halid@mail.ru>
2024-11-30 22:16:26 +0300

    added Seminar3:Task1

---
+++ Seminar3/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array1 = {1,8,4,19,5};
+int digit = 20;
+
+bool isExistNum = false;
+
+for (int i = 0; i < array1.Length; i++)
+{
+    if( array1[i] == digit)
+    {
+        isExistNum = true;
+        break;
+    }
+}
+//string result = isExistNum ? "Да" : "Нет";
+Console.WriteLine(isExistNum ? "Да" : "Нет");
