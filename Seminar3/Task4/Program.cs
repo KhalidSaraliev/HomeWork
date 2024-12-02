b3465883084841ab5bc5073a3485b398c56dd6c6
Saraliev Khalid <saraliev_halid@mail.ru>
2024-12-02 15:18:50 +0300

    added Seminar3:Task4

---
+++ Seminar3/Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿int number = 8741;
+int temp = number;
+int count = 0;
+
+while (temp != 0)
+{
+    temp /= 10;
+    count++;
+}
+
+int[] digits = new int[count];
+
+for (int i = count - 1; i >= 0; i--)
+{
+    digits[i] = number % 10;
+    number /= 10;
+}
+Console.Write('[');
+foreach(int digit in digits)
+{
+    Console.Write($"{digit} ");
+}
+Console.Write(']');
