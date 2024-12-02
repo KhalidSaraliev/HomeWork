99d9703f4bb5f4ac8a5635c2ae6dfe9170ff4f94
Saraliev Khalid <saraliev_halid@mail.ru>
2024-12-02 13:43:22 +0300

    added Seminar3:Task2

---
+++ Seminar3/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] arr = {9,1,5,6,4,8,3,22,10,66};
+int index = 0;
+int count = 0;
+
+while (index < arr.Length)
+{
+    if(arr[index] % 2 == 0)
+    {
+        count++;
+    }
+    index++;
+}
+
+Console.WriteLine($"Количество четных чисел в массиве {count} ");
