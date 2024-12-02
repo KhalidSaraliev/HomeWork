ec9e6bb153e0da9c578d25f0fbcf57b672fc43cf
Saraliev Khalid <saraliev_halid@mail.ru>
2024-12-02 14:51:32 +0300

    added Seminar3:Task3

---
+++ Seminar3/Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿double [] arr = {2.2,0.4,9.11,7.2,78.98};
+int index = 0;
+int index2 = 0;
+double maxNum = arr[index];
+double minNum = arr[index];
+
+while (index < arr.Length) // find minimun number
+{
+    if(arr[index] < minNum)
+    {
+        minNum = arr[index];
+    }
+    index++;
+}
+while (index2 < arr.Length)// find maximum number
+{
+    if (arr[index2] > maxNum)
+    {
+        maxNum = arr[index2];
+    }
+    index2++;
+}
+
+double result = maxNum - minNum;
+Console.WriteLine(result);
