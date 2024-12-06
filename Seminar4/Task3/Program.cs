8e50ff9e296c6cc347025a952eb518974d671420
Saraliev Khalid <saraliev_halid@mail.ru>
2024-12-06 04:28:50 +0300

    added seminar4:task3

---
+++ Seminar4/Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] array = {1,3,6,7,8,9,4};
+int temp;
+
+Console.Write("Исходный массив: ");
+foreach (int number in array)
+{
+    Console.Write(number + " ");
+}
+
+for (int i = 0; i < array.Length/2; i++)
+{
+    temp = array[i];
+    array[i] = array[array.Length-1-i];
+    array[array.Length -1 - i] = temp;
+}
+
+Console.Write("\nПеревернутый массив: ");
+foreach (int number in array)
+{
+    Console.Write(number + " ");
+}
