203108a3cdd983bc08a2fb3657b7afc0421552af
Saraliev Khalid <saraliev_halid@mail.ru>
2024-12-10 23:16:47 +0300

    added Seminar6:Task3

---
+++ Seminar6/Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿string input = "шалаш";
+
+bool IsPalindrome(string str)
+{
+    string normalized = new string(str.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+    return normalized.SequenceEqual(normalized.Reverse());
+}
+
+bool isPalindrome = IsPalindrome(input);
+Console.WriteLine(isPalindrome ? "Да" : "Нет");
