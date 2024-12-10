f3deb6963cc6c6ffbe9c724c3b39a9215eeeb31f
Saraliev Khalid <saraliev_halid@mail.ru>
2024-12-10 23:51:31 +0300

    add Seminar6:task4

---
+++ Seminar6/Task4/Program.cs
@@ -0,0 +1,11 @@
+﻿string input = "Hello my world"; // Входная строка со словами, разделенными пробелами
+
+string ReverseWords(string str) 
+{
+    string[] words = str.Split(' ');
+    Array.Reverse(words);
+    return string.Join(" ", words);
+}
+
+string result = ReverseWords(input); // Вызов метода для обращения порядка слов в строке
+Console.WriteLine(result); // Вывод результата
