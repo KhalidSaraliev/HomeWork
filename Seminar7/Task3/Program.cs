936817ef4179b7919e739341bd83da444e347f2a
Saraliev Khalid <saraliev_halid@mail.ru>
2024-12-12 02:10:40 +0300

    added seminar7:task3

---
+++ Seminar7/Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿void PrintArrayReverse(int[] array, int index)
+{
+    if (index < 0)
+    {
+        return;
+    }
+    Console.Write(array[index]);
+    PrintArrayReverse(array, index - 1);
+}
+
+int[] array = { 1, 2, 3, 4, 5 };
+Console.Write("Элементы массива, начиная с конца: ");
+PrintArrayReverse(array, array.Length - 1);
