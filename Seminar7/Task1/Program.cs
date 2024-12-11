01d3753a4767b30f95059d1c712678d65e4e8267
Saraliev Khalid <saraliev_halid@mail.ru>
2024-12-12 01:40:30 +0300

    added seminar7:task1

---
+++ Seminar7/Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿void PrintRange(int m, int n)
+{
+    if (m <= n)
+    {
+        Console.Write(m);
+        if (m < n)
+        {
+            Console.Write(",");
+            PrintRange(m + 1, n);
+        }
+    }
+    else
+    {
+        Console.Write(m);
+        if (m > n)
+        {
+           Console.Write(",");
+           PrintRange(m - 1, n); 
+        }
+    }
+}
+
+int m = 1;
+int n = 5;
+Console.WriteLine($"Числа промежутке: ");
+PrintRange(m, n);
